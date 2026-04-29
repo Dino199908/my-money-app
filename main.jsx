c11001af14a890ac53c30b052a56c17ae8325ac6
Dino199908 <kvym2fyxfm@privaterelay.appleid.com>
2026-04-28 23:19:36 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,8 +0,0 @@
-{
-  "name": "My Money Budget Tracker",
-  "short_name": "My Money",
-  "start_url": "/",
-  "display": "standalone",
-  "background_color": "#0a0a0a",
-  "theme_color": "#0a0a0a"
-}
